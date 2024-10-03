2f744b862009f8aca44bfc816f0d97fd389bf989
timvba <65532824+timvba@users.noreply.github.com>
2024-10-03 14:48:31 +0200

    yeet

--- line_hello_world/line_hello_world.ino
+++ line_hello_world/line_hello_world.ino
@@ -14,12 +14,8 @@
 //
 
 
-int AdirectionPin = 12;
 int ApwmPin = 3;
-int AbrakePin = 9;
-int BdirectionPin = 13;
 int BpwmPin = 11;
-int BbrakePin = 8;
 
 #include <Pixy2.h>
 
@@ -29,19 +25,14 @@ void setup()
 {
   Serial.begin(115200);
   Serial.print("Starting...\n");
-
-  pinMode(AdirectionPin, OUTPUT);
-  pinMode(ApwmPin, OUTPUT);
-  pinMode(AbrakePin, OUTPUT);
-  pinMode(BdirectionPin, OUTPUT);
-  pinMode(BpwmPin, OUTPUT);
-  pinMode(BbrakePin, OUTPUT);
-
   
-  digitalWrite(AbrakePin, LOW);
-  digitalWrite(BbrakePin, LOW);
-  digitalWrite(AdirectionPin, HIGH);
-  digitalWrite(BdirectionPin, HIGH);
+  //Setup Channel A
+  pinMode(12, OUTPUT); //Initiates Motor Channel A pin
+  pinMode(9, OUTPUT); //Initiates Brake Channel A pin
+
+  //Setup Channel B
+  pinMode(13, OUTPUT); //Initiates Motor Channel A pin
+  pinMode(8, OUTPUT);  //Initiates Brake Channel A pin
 
   pixy.init();
   // change to the line_tracking program.  Note, changeProg can use partial strings, so for example,
@@ -63,21 +54,22 @@ void loop()
     }
 //#########################
 
-analogWrite(ApwmPin, 254);
-analogWrite(BpwmPin, 254);
+  digitalWrite(12, HIGH);
+  digitalWrite(9, LOW);
+  analogWrite(3, 255);
+
+  digitalWrite(13, HIGH);
+  digitalWrite(8, LOW);
+  analogWrite(11, 255);
 
 delay(1000);
-
-analogWrite(ApwmPin, 0);
-analogWrite(BpwmPin, 0);
+ 
+  analogWrite(3, 0);
+  analogWrite(11, 0);
 
 delay(1000);
 
 //#########################
-    
-
-
-
 
 
 }
