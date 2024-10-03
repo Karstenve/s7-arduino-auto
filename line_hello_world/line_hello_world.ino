ac0589f6aeb58c34e1933e3403223485505daafd
Karstenve <karstenvevh@gmail.com>
2024-10-03 15:32:27 +0200

    Cleanup line tracking

--- line_hello_world/line_hello_world.ino
+++ line_hello_world/line_hello_world.ino
@@ -1,41 +1,46 @@
-//
-// begin license header
-//
-// This file is part of Pixy CMUcam5 or "Pixy" for short
-//
-// All Pixy source code is provided under the terms of the
-// GNU General Public License v2 (http://www.gnu.org/licenses/gpl-2.0.html).
-// Those wishing to use Pixy source code, software and/or
-// technologies under different licensing terms should contact us at
-// cmucam@cs.cmu.edu. Such licensing terms are available for
-// all portions of the Pixy codebase presented here.
-//
-// end license header
-//
+// Motor A Pins
+#define MotorASpeedPin 3
+#define MotorADirectionPin 12
+#define MotorABrakePin 9
 
+// Motor B Pins
+#define MotorBSpeedPin 11
+#define MotorBDirectionPin 13
+#define MotorBBrakePin 8
 
-int ApwmPin = 3;
-int BpwmPin = 11;
+// Motor A Constants
+int MotorASpeed = 255;
+int MotorADirection = HIGH;
+int MotorABrake = LOW;
 
-#include <Pixy2.h>
+// Motor B Constants
+int MotorBSpeed = 255;
+int MotorBDirection = HIGH;
+int MotorBBrake = LOW;
 
-Pixy2 pixy;
+// #include <Pixy2.h>
+
+// Pixy2 pixy;
 
 void setup()
 {
+  // Init Serial
   Serial.begin(115200);
   Serial.print("Starting...\n");
   
+  // Init Motor B
+  analogWrite(MotorASpeedPin, MotorASpeed);
+  pinMode(MotorADirectionPin, OUTPUT); //Initiates Motor Channel A direction pin
+  digitalWrite(MotorADirectionPin, MotorADirection);
+  pinMode(MotorABrakePin, OUTPUT); //Initiates Motor Channel A brake pin
+  digitalWrite(MotorABrakePin, MotorABrake);
 
-  pinMode(12, OUTPUT); //Initiates Motor Channel A direction pin
-  digitalWrite(12, HIGH);
-  pinMode(9, OUTPUT); //Initiates Motor Channel A brake pin
-  digitalWrite(9, LOW);
-
-  pinMode(13, OUTPUT); //Initiates Motor Channel B direction pin
-  digitalWrite(13, HIGH);
-  pinMode(8, OUTPUT); //Initiates Motor Channel B brake pin
-  digitalWrite(8, LOW);
+  // Init Motor B
+  analogWrite(MotorBSpeedPin, MotorBSpeed);
+  pinMode(MotorBDirectionPin, OUTPUT); //Initiates Motor Channel B direction pin
+  digitalWrite(MotorBDirectionPin, MotorBDirection);
+  pinMode(MotorBBrakePin, OUTPUT); //Initiates Motor Channel B brake pin
+  digitalWrite(MotorBBrakePin, MotorBBrake);
 
   //pixy.init();
   // change to the line_tracking program.  Note, changeProg can use partial strings, so for example,
@@ -47,20 +52,6 @@ void setup()
 
 void loop()
 {
- // int8_t i;
- // char buf[128];
- 
-  // pixy.line.getMainFeatures(1,false);
-
- // if (pixy.line.numVectors) {
- //   pixy.line.vectors->print();
- //   }
-//#########################
-
-  analogWrite(ApwmPin, 255); //motor A go brrt
-  analogWrite(BpwmPin, 255);
-
-//#########################
 
 
 }
